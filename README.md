f0ae86e3d2f93127a35d49a5a0fe4bd6b0bb1d59
hyunsangwon <hyunsangwon93@gmail.com>
2022-12-09 12:27:21 +0900

    feat : 확장프로그램 정리

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ### jpa
 JPA 수업 😴 22.12.08 ~ 
 
-
 #### 참고사이트
 - https://covenant.tistory.com/243
+
+### VSCode 필요한 확장프로그램
+- Sprinb Boot Tools
+- Gradle for Java
+- Spring Boot Snippets
+- Spring Boot Dashboard
--- src/main/java/com/dw/jpa/controller/UserController.java
+++ src/main/java/com/dw/jpa/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.dw.jpa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @GetMapping("/hello")
+    public String callHello(){
+        return "hello world";
+    }
+    
 }
