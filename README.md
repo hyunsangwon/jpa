69051cb8bf0aa18bd39e63f4512651963c8bd478
hyunsangwon <hyunsangwon93@gmail.com>
2022-12-13 12:47:12 +0900

    addd

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 ### jpa
 JPA 수업 😴 22.12.08 ~ 
 
-#### 참고사이트
-- https://www.bezkoder.com/spring-boot-jpa-crud-rest-api/
-
 ### VSCode 필요한 확장프로그램
 - Sprinb Boot Tools
 - Gradle for Java
 - Spring Boot Snippets
 - Spring Boot Dashboard
+
+### 프로젝트 구조
+```bash
+├── src
+│   ├── controller
+│   ├── service
+│   ├── repository
+│   └── model
+├── resources
+    └── application.yaml
+``` 
+
+#### 참고사이트
+- https://www.bezkoder.com/spring-boot-jpa-crud-rest-api
--- src/main/java/com/dw/jpa/model/Users.java
+++ src/main/java/com/dw/jpa/model/Users.java
@@ -3,6 +3,7 @@ package com.dw.jpa.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,7 +14,7 @@ import javax.persistence.Table;
 public class Users {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
     private Long id;
     @Column(name="name")
